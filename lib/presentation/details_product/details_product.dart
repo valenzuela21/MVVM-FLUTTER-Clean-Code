c4e91cf0850d14256398ebbb7124508c7af26c02
valenzuela21 <vlzdavid12@outlook.com>
2024-11-17 12:12:45 -0500

    MVVM Flutter

--- lib/presentation/details_product/details_product.dart
+++ lib/presentation/details_product/details_product.dart
@@ -1,8 +1,10 @@
+import 'package:approducts/presentation/common/components/start_rating.dart';
 import 'package:approducts/presentation/resources/color_manager.dart';
 import 'package:approducts/presentation/resources/strings_manager.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
+import '../resources/assets_manager.dart';
 import '../resources/routes_manager.dart';
 import '../resources/style_manager.dart';
 import '../resources/values_manager.dart';
@@ -22,8 +24,9 @@ class _DetailsProductViewState extends State<DetailsProductView> {
       appBar: AppBar(
         backgroundColor: ColorManager.primary,
         title: Text(AppStrings.viewDetailProduct,
-            style: getMediumStyle(
-                color: ColorManager.white, fontSize: AppSize.s20)).tr(),
+                style: getMediumStyle(
+                    color: ColorManager.white, fontSize: AppSize.s20))
+            .tr(),
         leading: IconButton(
           icon: Icon(Icons.arrow_back, color: ColorManager.white),
           onPressed: () {
@@ -36,12 +39,60 @@ class _DetailsProductViewState extends State<DetailsProductView> {
         children: [
           Row(
             children: [
-              Text("Iphone X", style: getMediumStyle(color: ColorManager.primary, fontSize: AppSize.s28))
-
+              Flexible(
+                flex: 1,
+                child: Text("Iphone X",
+                    style: getMediumStyle(
+                        color: ColorManager.primary, fontSize: AppSize.s28)),
+              ),
+              Flexible(flex: 2, child: StartRatingComponent())
             ],
-          )
+          ),
+          Text("Descripción",
+              style: getMediumStyle(
+                  color: ColorManager.primary, fontSize: AppSize.s28)),
+          Text(
+              "Esta es una descripción del producto de apple que es muy lindo.",
+              style: getRegularStyle(
+                  color: ColorManager.primary, fontSize: AppSize.s18)),
+          _ListPrices()
         ],
       )),
+    );
+  }
+}
+
+class _ListPrices extends StatelessWidget {
+  const _ListPrices({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Text("Marca: ", style: getBoldStyle(color: ColorManager.primary, fontSize: AppSize.s20)),
+            Image.asset(
+              ImageAssets.clip,
+              width: AppSize.s28,
+              height: AppSize.s28,
+            ),
+            Text("Apple", style: getRegularStyle(color: ColorManager.primary, fontSize: AppSize.s18))
+          ],
+        ),
+        Row(
+          children: [
+            Text("Precio: ", style: getBoldStyle(color: ColorManager.primary, fontSize: AppSize.s20)),
+            Text("\$ 899", style: getRegularStyle(color: ColorManager.primary, fontSize: AppSize.s18))
+          ],
+        ),
+        Row(
+          children: [
+            Text("Categoria: ", style: getBoldStyle(color: ColorManager.primary, fontSize: AppSize.s20)),
+            Text("SmartPhone", style: getRegularStyle(color: ColorManager.primary, fontSize: AppSize.s18))
+          ],
+        )
+      ],
     );
   }
 }
