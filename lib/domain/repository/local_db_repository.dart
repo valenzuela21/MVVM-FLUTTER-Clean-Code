5878eafe44894d12cfff3632c14e841bc5919be5
valenzuela21 <vlzdavid12@outlook.com>
2024-11-17 23:30:48 -0500

    MVVM Flutter

--- lib/domain/repository/local_db_repository.dart
+++ lib/domain/repository/local_db_repository.dart
@@ -1,71 +1,129 @@
 import 'package:sqflite/sqflite.dart';
+import 'package:logger/logger.dart';
 import '../../data/data_resource/local_data_sqlift.dart';
 
-class LocalRepositoryDatabase {
+final logger = Logger();
 
+class LocalRepositoryDatabase {
   Future<int> insertBrand(Map<String, dynamic> brand) async {
-    final db = await DatabaseHelper.instance.database;
-    return await db.insert('brand', brand);
+    try {
+      final db = await DatabaseHelper.instance.database;
+      return await db.insert('brand', brand);
+    } catch (e) {
+      logger.e("Error: $e");
+      return -1;
+    }
   }
 
   Future<int> insertCategory(Map<String, dynamic> category) async {
-    final db = await DatabaseHelper.instance.database;
-    return await db.insert('category', category);
+    try {
+      final db = await DatabaseHelper.instance.database;
+      return await db.insert('category', category);
+    } catch (e) {
+      logger.e("Error: $e");
+      return -1;
+    }
   }
 
   Future<int> insertProduct(Map<String, dynamic> product) async {
-    final db = await DatabaseHelper.instance.database;
-    return await db.insert('product', product);
+    try {
+      final db = await DatabaseHelper.instance.database;
+      return await db.insert('product', product);
+    } catch (e) {
+      logger.e("Error: $e");
+      return -1;
+    }
   }
 
   Future<List<Map<String, dynamic>>> getProducts() async {
-    final db = await DatabaseHelper.instance.database;
-    var result = await db.rawQuery('''
+    try {
+      final db = await DatabaseHelper.instance.database;
+      var result = await db.rawQuery('''
       SELECT product.id, product.name AS product_name, product.description, product.price, product.rating,
              brand.name AS brand_name, category.name AS category_name
       FROM product
       LEFT JOIN brand ON product.brand_id = brand.id
       LEFT JOIN category ON product.category_id = category.id
     ''');
-    return result;
+      return result;
+    } catch (e) {
+      logger.e("Error: $e");
+      return [];
+    }
   }
 
   Future<int> getProductCount() async {
-    final db = await DatabaseHelper.instance.database;
-    var result = await db.rawQuery('''
+    try {
+      final db = await DatabaseHelper.instance.database;
+      var result = await db.rawQuery('''
     SELECT COUNT(*) FROM product
     ''');
-    int count = Sqflite.firstIntValue(result) ?? 0;
-    return count;
+      int count = Sqflite.firstIntValue(result) ?? 0;
+      return count;
+    } catch (e) {
+      logger.e("Error: $e");
+      return -1;
+    }
   }
 
   Future<List<Map<String, dynamic>>> getBrands() async {
-    final db = await DatabaseHelper.instance.database;
-    return await db.query('brand');
+    try {
+      final db = await DatabaseHelper.instance.database;
+      return await db.query('brand');
+    } catch (e) {
+      logger.e("Error: $e");
+      return [];
+    }
   }
 
   Future<List<Map<String, dynamic>>> getCategories() async {
-    final db = await DatabaseHelper.instance.database;
-    return await db.query('category');
+    try {
+      final db = await DatabaseHelper.instance.database;
+      return await db.query('category');
+    } catch (e) {
+      logger.e("Error: $e");
+      return [];
+    }
   }
 
   Future<int> deleteProduct(int id) async {
-    final db = await DatabaseHelper.instance.database;
-    return await db.delete('product', where: 'id = ?', whereArgs: [id]);
+    try {
+      final db = await DatabaseHelper.instance.database;
+      return await db.delete('product', where: 'id = ?', whereArgs: [id]);
+    } catch (e) {
+      logger.e("Error: $e");
+      return -1;
+    }
   }
 
   Future<int> deleteBrand(int id) async {
-    final db = await DatabaseHelper.instance.database;
-    return await db.delete('brand', where: 'id = ?', whereArgs: [id]);
+    try {
+      final db = await DatabaseHelper.instance.database;
+      return await db.delete('brand', where: 'id = ?', whereArgs: [id]);
+    } catch (e) {
+      logger.e("Error: $e");
+      return -1;
+    }
   }
 
   Future<int> deleteCategory(int id) async {
-    final db = await DatabaseHelper.instance.database;
-    return await db.delete('category', where: 'id = ?', whereArgs: [id]);
+    try {
+      final db = await DatabaseHelper.instance.database;
+      return await db.delete('category', where: 'id = ?', whereArgs: [id]);
+    } catch (e) {
+      logger.e("Error: $e");
+      return -1;
+    }
   }
 
   Future<int> updateProduct(Map<String, dynamic> product) async {
-    final db = await DatabaseHelper.instance.database;
-    return await db.update('product', product, where: 'id = ?', whereArgs: [product['id']]);
+    try {
+      final db = await DatabaseHelper.instance.database;
+      return await db.update('product', product,
+          where: 'id = ?', whereArgs: [product['id']]);
+    } catch (e) {
+      logger.e("Error: $e");
+      return -1;
+    }
   }
 }
